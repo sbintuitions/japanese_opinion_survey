5f44b875e96f7243b7693a3436114da33edb1dd1
masaya-ohagi <masaya.ohagi@sbintuitions.co.jp>
2024-06-10 18:02:26 +0900

    fixed prompt

--- src/prompt_creators.py
+++ src/prompt_creators.py
@@ -1,14 +1,7 @@
-from src.items import QA
-
-
-def create_japanese_opinion_survey_prompt(qa: QA, labels: list[str], permutation: list[int]) -> str:
-    prompt = f"""{qa.prefix}
-
-{qa.question}
-
+def create_japanese_opinion_survey_prompt(question: str, choices: list[str], labels: list[str], permutation: list[int]) -> str:
+    prompt = f"""質問: {question}
 """
-    for i in range(len(qa.choices)):
-        prompt += f"{labels[i]} {qa.choices[permutation[i]].content}\n"
-    prompt += "\n"
+    for i in range(len(choices)):
+        prompt += f"{labels[i]}: {choices[permutation[i]]}\n"
     prompt += "回答: "
     return prompt
