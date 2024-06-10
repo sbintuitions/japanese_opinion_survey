055d2983b291a3e589eda95261ec0bba6a7af49c
masaya-ohagi <masaya.ohagi@sbintuitions.co.jp>
2024-06-10 18:02:11 +0900

    adapted to japanion qa

--- src/load_data.py
+++ src/load_data.py
@@ -1,20 +1,21 @@
 from ast import literal_eval
-from typing import List
 
 import pandas as pd
 
 from src.items import QA, Choice
 
 
-def load_japanese_opinion_survey(file_path: str, include_reject: bool, include_succession: bool) -> List[QA]:
+def load_japanese_opinion_survey(file_path: str, include_reject: bool, include_succession: bool) -> list[QA]:
     df = pd.read_csv(file_path)
     qas = []
     if include_reject:
         reject_choices = []
     else:
         reject_choices = ["わからない", "無回答"]
-    for prefix, is_succession, question, choices, distributions, year, topic, question_class in zip(df["prefix"], df["is_succession"], df["question"], df["choices"], df["distributions"], df["year"], df["topic"], df["question_class"]):
-        if not include_succession and is_succession == "YES":
+    for prefix, is_succession, question, choices, distributions, year, topic, question_class, question_type in zip(df["Context information"], df["parent_question"], df["Question"], df["choices"], df["distributions"], df["year"], df["topic"], df["question_class"], df["question_type"]):
+        if not include_succession and isinstance(is_succession, str):
+            continue
+        if question_type != "SA":
             continue
         contents = literal_eval(choices)
         float_distributions = literal_eval(distributions)
@@ -23,5 +24,5 @@ def load_japanese_opinion_survey(file_path: str, include_reject: bool, include_s
             if content in reject_choices:
                 continue
             choices.append(Choice(content=content, real_distribution=distribution, predicted_distributions=[]))
-        qas.append(QA(prefix=prefix, question=question, choices=choices, year=year, topic=topic, question_class=question_class))
+        qas.append(QA(prefix=prefix, question=question, choices=choices, year=year, topic=topic, question_class=question_class, question_type=question_type))
     return qas
