f632e4c25337ee534ca79ec7a9ab25dabf408d38
masaya-ohagi <masaya.ohagi@sbintuitions.co.jp>
2024-06-10 18:01:38 +0900

    added all permutations

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,9 @@
-def make_permutations(qa) -> list[int]:
+import itertools
+
+def make_permutations(qa, all_permutations: bool = False) -> list[int]:
     choice_length = len(qa.choices)
-    permutations = [[(i + j) % choice_length for j in range(choice_length)] for i in range(choice_length)]
+    if all_permutations:
+        permutations = list(itertools.permutations([i for i in range(choice_length)], choice_length))
+    else:
+        permutations = [[(i + j) % choice_length for j in range(choice_length)] for i in range(choice_length)]
     return permutations
