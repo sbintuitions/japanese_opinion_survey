1a69421190bca04f733b09f6d090090be6294915
masaya-ohagi <masaya.ohagi@sbintuitions.co.jp>
2024-06-10 18:01:24 +0900

    adapted to flexeval

--- src/items.py
+++ src/items.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-from typing import Optional, List
+from flexeval.core.multiple_choice_dataset.base import MultipleChoiceDataset, MultipleChoiceInstance
 
 
 @dataclass
 class Choice:
     content: str
     real_distribution: float
-    predicted_distributions: List[float]
+    predicted_distributions: list[float]
 
 
 @dataclass
@@ -17,3 +17,24 @@ class QA:
     year: str
     topic: str
     question_class: str
+    question_type: str
+
+
+class MyChoiceDataset(MultipleChoiceDataset):
+    def __init__(
+        self,
+        qas
+    ):
+        self._qas = qas
+
+    def __len__(self) -> int:
+        return len(self._qas)
+
+    def __getitem__(self, i: int) -> MultipleChoiceInstance:
+        question = self._qas[i]
+        
+        return MultipleChoiceInstance(
+            inputs={"question": question.question},
+            choices=[choice.content for choice in question.choices],
+            correct_choice_index=0,
+        )
